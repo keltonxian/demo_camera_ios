729d8057ffbd4dc0a31fca442dba3ff9ab0aa2a0
keltonxian <keltonxian@hotmail.com>
2016-11-06 11:54:43 +0800

    update

--- CameraDemo/TakePhotoViewController.m
+++ CameraDemo/TakePhotoViewController.m
@@ -17,12 +17,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self showTakePhotoView];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)showTakePhotoView {
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = (id)self;
+    picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    picker.showsCameraControls = NO;
+    [self presentViewController:picker animated:YES completion:^(void) {
+        
+    }];
 }
 
 
